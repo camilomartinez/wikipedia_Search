b5a670b7478ee9cf32e546525708a51bc2773876
Hasan_sh <hasansh343@gmail.com>
2016-09-22 16:50:21 +0200

    JS file done.

--- javascript.js
+++ javascript.js
@@ -1,40 +1,32 @@
-$(document).ready(function () {
-	// body...
-	$('#result').click(function(){
 
-		var result = $('#submit').val(); //the value of the input 
-		var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ result +"&format=json&callback=?"; // the api to search on wikipedia
-		console.log(api);
-
+$(document).ready(function(){
+	$("#getArticle").submit(function(event){
+		event.preventDefault();
+    var submit = $('#submit').val();//to get the value of input field
+var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ submit +"&format=json&callback=?";
+                  //the wikipedia API
+//console.log(api);
 		$.ajax({
-			type:'GET',
-			url:api,
-			async:false,
+			type: "GET",
+			url: api,
+			contentType: "application/json; charset=utf-8",
+			async: false,
 			dataType: "json",
 			success: function(data){
-				console.log(data)
+				// console.log(data[1][1]+' '+data[3][0])
+				$('#wikiResult').html('');//to stop repeating the result..
 				for (var i =0; i <data[1].length; i++) {
-					
-					$('#wikiResult').html(' <a href="'+data[3][i]+'"><h2>'+data[1][i]+'</h2></a>'+'<p>'+data[2][i]+'</p>')
+					$('#wikiResult').prepend('<h2 class="headResult">'+data[1][i]+'<br><a id="anchorTag" href="'+data[3][i]+'" target="_blank">'+data[2][i]+'</a></h2>')
 				}
-
+				$('#submit').val('');//here to delete the text after applying the function(after getting the result)
 			},
 			error: function(errorMessage){
-				alert('there is some Error..!')
+				alert('you have an error');
 			}
 		})
 
-
-
-
-
-
 	});
-
-})
-
-
-// Heading
+});
 				// console.log(data[1][0]);
 
 				// Description
