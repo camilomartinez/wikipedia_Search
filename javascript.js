4140c0af736df18bd104bbba989d8f465f673757
Camilo Martinez <marscamand@gmail.com>
2016-09-24 23:42:14 +0200

    Fixing api call
    Comment your name, proper indentation and append to keep order

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
-Coded By Hasan_Sh
+//Coded By Hasan_Sh
 $(document).ready(function(){
 	$("#getArticle").submit(function(event){
 		event.preventDefault();
-    var submit = $('#submit').val();//to get the value of input field
-var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ submit +"&format=json&callback=?";
-                  //the wikipedia API
-//console.log(api);
+		var submit = $('#submit').val();//to get the value of input field
+		var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ submit +"&format=json&callback=?";
+        //the wikipedia API
+		//console.log(api);
 		$.ajax({
 			type: "GET",
 			url: api,
@@ -16,7 +16,7 @@ var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ submit
 				// console.log(data[1][1]+' '+data[3][0])
 				$('#wikiResult').html('');//to stop repeating the result..
 				for (var i =0; i <data[1].length; i++) {
-					$('#wikiResult').prepend('<h2 class="headResult">'+data[1][i]+'<br><a id="anchorTag" href="'+data[3][i]+'" target="_blank">'+data[2][i]+'</a></h2>')
+					$('#wikiResult').append('<h2 class="headResult">'+data[1][i]+'<br><a id="anchorTag" href="'+data[3][i]+'" target="_blank">'+data[2][i]+'</a></h2>')
 				}
 				$('#submit').val('');//here to delete the text after applying the function(after getting the result)
 			},
